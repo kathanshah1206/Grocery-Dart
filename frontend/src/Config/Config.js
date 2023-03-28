09c0d7290cba393a97f88d3d7033e4c0bb6f1113
kathanshah1206 <97287116+kathanshah1206@users.noreply.github.com>
2023-03-28 17:10:08 +0530

    Initial commit

---
+++ frontend/src/Config/Config.js
@@ -0,0 +1,6 @@
+export const CLIENT_ID =
+  process.env.CLIENT_ID ||
+  "AY6ja2WFMk0h0IcRoaQ2ZYxLHxPYQq7em_B1llQbGQ6RjP-_MLah7mezcby25Gi4PVj0OHOJwbXRZzHe";
+export const APP_SECRET =
+  process.env.APP_SECRET ||
+  "EK7cX0jubCRJatuwpY8TfGM-OWHg5CACwy_yUJ5sGLbHNzfsf2kbs2unOlJoSUIxzkxXj-xXWwiMXHZJ";
